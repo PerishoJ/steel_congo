2cbe924f4a03a0a990690195cdb4fc03cabc17ef
joe <cafejoe61@google.com>
2023-09-13 23:12:01 -0500

    added very basic hitbox and hurtbox code. No damage system yet.

---
+++ bot/HurtBox.gd
@@ -0,0 +1,31 @@
+class_name HurtBox
+extends Area3D
+
+var debug_lbl: Label3D;
+var lbl_reset_delay_millis = 2000;
+var lbl_reset_time = 0;
+var orig_lbl_text: String;
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	self.area_entered.connect(_on_area_entered)
+	debug_lbl = owner.get_node("_debug_label_name");
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if shouldResetLbl() and "OUCH!!!"==debug_lbl.text:
+		debug_lbl.text = orig_lbl_text
+
+
+func _on_area_entered( hitbox: HitBox):
+	print("hurt")
+	if(debug_lbl!=null):
+		if shouldResetLbl():
+			orig_lbl_text = debug_lbl.text # use this to set the label back later
+			print("Setting label to " + orig_lbl_text)
+		debug_lbl.text = "OUCH!!!"
+		lbl_reset_time = Time.get_ticks_msec() + lbl_reset_delay_millis;
+
+func shouldResetLbl():
+	return  Time.get_ticks_msec() > lbl_reset_time 
